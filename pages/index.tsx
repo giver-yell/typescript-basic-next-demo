1837712af82e2905a028f54a87fbccaf08f0cb48
giver.yell <giver.yell@gmail.com>
2023-11-06 08:28:54 +0900

    ✨ eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
-import { GetStaticProps, NextPage } from 'next';
-import { Post } from '@/types/post';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps, NextPage } from "next";
+import { Post } from "@/types/post";
 
 
 
@@ -22,11 +22,11 @@ const Home: NextPage<Props> = ({ allPostsData }) => {
       <section className={utilStyles.headingMd}>
         <p>
           Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
-          translator (English/Japanese). You can contact me on{' '}
+          translator (English/Japanese). You can contact me on{" "}
           <a href="https://twitter.com/chibicode">Twitter</a>.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -46,7 +46,7 @@ const Home: NextPage<Props> = ({ allPostsData }) => {
       </section>
     </Layout>
   );
-}
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
@@ -55,7 +55,7 @@ export const getStaticProps: GetStaticProps = async () => {
       allPostsData,
     },
   };
-}
+};
 
 
 export default Home;
