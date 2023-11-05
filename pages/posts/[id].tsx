2499cddf5c63ffd9ea33e65c39bdde0fe7cf2140
giver.yell <giver.yell@gmail.com>
2023-11-05 15:34:39 +0900

    ✨ typescript

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,54 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+import { GetStaticPaths, NextPage } from 'next';
+import { Post } from '@/types/post';
+import { GetStaticProps } from 'next';
+
+type Props = {
+  postData: Post
+}
+
+const Post: NextPage<Props> = ({ postData }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths<Pick<Post, "id">> = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps<Props, { id: string }> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default Post;
